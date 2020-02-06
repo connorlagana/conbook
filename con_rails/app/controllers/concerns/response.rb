2c64d05682a4339a8844efb31d3f0bf3f529db0c
Connor Lagana <connor.lagana@gmail.com>
2020-02-06 14:05:26 -0500

    exception handler and response

--- con_rails/app/controllers/concerns/response.rb
+++ con_rails/app/controllers/concerns/response.rb
@@ -1,3 +1,5 @@
+#Remember, our API returns json_response (JSON and an HTTP status code 200 by default).
+
 module Response
   def json_response(object, status = :ok)
     render json: object, status: status
