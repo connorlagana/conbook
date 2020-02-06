cf47e066a2ecd18af74e8e0cbb49496dc9f85914
Connor Lagana <connor.lagana@gmail.com>
2020-02-06 13:41:07 -0500

    finalized models

--- con_rails/app/models/comment.rb
+++ con_rails/app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ApplicationRecord
+  # model association
   belongs_to :post
+
+  # validation
+  validates_presence_of :name
 end
--- con_rails/app/models/user.rb
+++ con_rails/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  # encrypt password
+  has_secure_password
+
+  # Model associations
+  has_many :posts, foreign_key: :author
+  # Validations
+  validates_presence_of :name, :email, :password_digest
 end
